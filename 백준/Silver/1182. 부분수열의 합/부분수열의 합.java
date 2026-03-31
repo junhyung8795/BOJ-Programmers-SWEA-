edbe6951273ed20146843e9dfcded39182c23ec3
박준형 <109512586+junhyung8795@users.noreply.github.com>
2026-03-31 11:48:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 200 ms, Memory: 15152 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,54 +1,34 @@
 import java.io.*;
 import java.util.*;
 
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-public class Main {
-	static int N;
-	static int S;
-	static int answer;
-	static int[] nums;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] inputNS = br.readLine().split(" ");
-		N = Integer.parseInt(inputNS[0]);
-		S = Integer.parseInt(inputNS[1]);
-		
-		nums = new int[N];
-		String[] inputNums = br.readLine().split(" ");
-		for(int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(inputNums[i]);
-		}
-		
-		
-		List<Integer> list = new ArrayList<>();
-		answer = 0;
-		for(int i = 0 ; i <N ;i++) {
-			list.add(nums[i]);
-			backtracking(list, i + 1, nums[i]);
-			list.remove(list.size() - 1);
-		}
-		
-		System.out.println(answer);
-		
-	}
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int S = Integer.parseInt(s[1]);
+        int answer = 0;
+        int[] nums = new int[N];
+        String[] ip = br.readLine().split(" ");
+        for(int i = 0; i < N; i++){
+            nums[i] = Integer.parseInt(ip[i]);
+        }
+        // System.out.println(Arrays.toString(nums));
 
-	private static void backtracking(List<Integer> list, int startIndex, int sum) {
-		if(startIndex == N) {
-			
-			if(sum == S) {
-				answer += 1;
-			}
-			return;
-		}
-		
-		
-		list.add(nums[startIndex]);
-		backtracking(list, startIndex + 1 ,sum + nums[startIndex]);
-		list.remove(list.size() - 1);
-		backtracking(list, startIndex + 1, sum);
+        for(int mask = 1;  mask < (1 << N) ; mask++){
+            int candi = 0;
+            for(int j = N - 1 ; j >= 0; j--){
+                if((mask & (1 << j)) != 0){
+                    candi += nums[j];
+                    
+                }
+            }
+            if(candi == S){
+                answer += 1;
+            }
+        }
 
-		
-		
-	}
+        System.out.println(answer);
+    }
 }
